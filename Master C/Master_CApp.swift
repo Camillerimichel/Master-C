c536e49d09f9c3cf571c3269138ebe7d4b8558d7
Camillerimichel <70330066+Camillerimichel@users.noreply.github.com>
2025-08-14 09:12:21 +0200

    Initial Commit

---
+++ Master C/Master_CApp.swift
@@ -0,0 +1,17 @@
+//
+//  Master_CApp.swift
+//  Master C
+//
+//  Created by Michel Camilleri on 14/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Master_CApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Master CTests/Master_CTests.swift
@@ -0,0 +1,17 @@
+//
+//  Master_CTests.swift
+//  Master CTests
+//
+//  Created by Michel Camilleri on 14/08/2025.
+//
+
+import Testing
+@testable import Master_C
+
+struct Master_CTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
